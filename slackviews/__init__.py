be175abed16f27526f809bfc66e9ede41c6a0e74
Agustin Escamez <escamez@users.noreply.github.com>
2020-09-18 19:33:47 +0200

    Update __init__.py
    Adding interface for pypi's package interface

---
+++ slackviews/__init__.py
@@ -0,0 +1,3 @@
+from slackviews.view import AbstractBlock, AbstractBuilder, AbstractText, PlainText, MarkDown, Header, Image, \
+    Confirmation, Button, Option, OptionGroup, SelectMenu, MultiSelectMenu, Overflow, PlainTextInput, Section, \
+    Divider, Actions, Context, Input, View, Modal, Home, BlocksArray, BlocksFactory
