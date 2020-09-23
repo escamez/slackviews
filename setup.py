973ad7e7b21e5090fd1af4807c9f82d4bb97fb9f
escamez <aech22@gmail.com>
2020-09-23 18:51:03 +0200

    mend

--- setup.py
+++ setup.py
@@ -9,15 +9,19 @@ from setuptools import setup, find_packages
 __author__ = 'agutin.escamezchimeno@telefonica.com'
 
 
-def _read_file(filename, lines=True):
+def _read_file(filename, lines=True, non_empty=False):
     content = open(filename, 'r').read()
     if lines:
-        return [line.rstrip(' ') for line in content.split('\n')]
+        _lines = [line.rstrip(' ') for line in content.split('\n')]
+        if non_empty:
+            _lines = list(filter(lambda l: l != '', _lines))
+        return _lines
     return content
 
 
 def _version():
-    return _read_file('VERSION', lines=True)[0].rstrip(' ')
+    return _read_file('VERSION', lines=True, non_empty=True)[0].rstrip(' ')
+
 
 setup(
     name='slackviews',
@@ -33,8 +37,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    keywords=_read_file('KEYWORDS'),
+    keywords=_read_file('KEYWORDS', non_empty=True),
     install_requires=[],
-    classifiers=_read_file('CLASSIFIERS'),
+    classifiers=_read_file('CLASSIFIERS', non_empty=True),
     python_requires='>=3.6'
 )
